7c342d12975ffedd553123d36c8ba43fdeba1261
RimlTempest <riml.slime@gmail.com>
2022-11-03 08:21:03 +0900

    fix: router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld />
+  <header>
+    <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+    </div>
+  </header>
+  <v-app>
+    <v-main>
+      <RouterView />
     </v-main>
   </v-app>
-  <RouterView />
 </template>
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../pages/HomeView.vue";
+import HelloWorld from "@/pages/HelloWorld.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: HelloWorld,
     },
     {
       path: "/about",
